93dcfd6ae81ea75f688b068f69a2f82621b9e477
Giwoo Lee <gglee89@gmail.com>
2024-09-21 12:12:01 +0700

    feat: add graphql types

--- server.js
+++ server.js
@@ -1,8 +1,44 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+const allCourses = [
+  {
+    id: "book-06",
+    name: "TypeScript Basics",
+    description: "Typescript Basics for beginners",
+    price: 599.99,
+    discount: false,
+  },
+  {
+    id: "book-07",
+    name: "GraphQL Basics",
+    description: "GraphQL Basics for beginners",
+    price: 499.99,
+    discount: true,
+  },
+  {
+    id: "book-08",
+    name: "NextJS Basics",
+    description: "NextJS Basics for beginners",
+    price: 599.99,
+    discount: false,
+  },
+];
+
 const typeDefs = gql`
   type Query {
-    welcome: String
+    welcome: String!
+    numOfCourses: Int
+    courses: [Course!]!
+    course(id: ID!): Course
+    price: Float
+    isTrainer: Boolean
+  }
+
+  type Course {
+    name: String!
+    description: String!
+    price: Float!
+    discount: Boolean!
   }
 `;
 
@@ -10,6 +46,22 @@ const resolvers = {
   Query: {
     welcome: () => {
       return "Welcome to the World of GraphQL";
+    },
+    numOfCourses: () => {
+      return 12;
+    },
+    courses: () => allCourses,
+    course: (parent, args, context) => {
+      const courseId = args.id;
+      const course = allCourses.find((course) => course.id === courseId);
+      if (!course) return null;
+      return course;
+    },
+    price: () => {
+      return 1465.98;
+    },
+    isTrainer: () => {
+      return true;
     },
   },
 };
