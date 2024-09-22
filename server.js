de72770ad9a5911164b4d2d837c21ef44cf4afec
Giwoo Lee <gglee89@gmail.com>
2024-09-22 09:38:04 +0700

    feat: add extra courses

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+const genres = [
+  { id: "cat-01", name: "Technical" },
+  { id: "cat-02", name: "History" },
+];
+
 const allCourses = [
   {
     id: "book-06",
@@ -7,6 +12,7 @@ const allCourses = [
     description: "Typescript Basics for beginners",
     price: 599.99,
     discount: false,
+    genreId: "cat-01",
   },
   {
     id: "book-07",
@@ -14,6 +20,7 @@ const allCourses = [
     description: "GraphQL Basics for beginners",
     price: 499.99,
     discount: true,
+    genreId: "cat-01",
   },
   {
     id: "book-08",
@@ -21,6 +28,31 @@ const allCourses = [
     description: "NextJS Basics for beginners",
     price: 599.99,
     discount: false,
+    genreId: "cat-01",
+  },
+  {
+    id: "book-21",
+    name: "The Immortals of Meluha",
+    description: "Shiva Trilogy -1",
+    price: 299.99,
+    discount: false,
+    genreId: "cat-02",
+  },
+  {
+    id: "book-22",
+    name: "The Secret of the Nagas",
+    description: "Shiva Trilogy -2",
+    price: 199.99,
+    discount: true,
+    genreId: "cat-02",
+  },
+  {
+    id: "book-23",
+    name: "The Oath of the Vayaputras",
+    description: "Shiva Trilogy -3",
+    price: 599.99,
+    discount: false,
+    genreId: "cat-02",
   },
 ];
 
@@ -30,6 +62,8 @@ const typeDefs = gql`
     numOfCourses: Int
     courses: [Course!]!
     course(id: ID!): Course
+    genres: [Genre!]!
+    genre(catId: ID!): Genre
     price: Float
     isTrainer: Boolean
   }
@@ -39,6 +73,13 @@ const typeDefs = gql`
     description: String!
     price: Float!
     discount: Boolean!
+    genreId: ID!
+  }
+
+  type Genre {
+    id: ID!
+    name: String!
+    courses: [Course!]!
   }
 `;
 
@@ -57,11 +98,24 @@ const resolvers = {
       if (!course) return null;
       return course;
     },
+    genres: () => genres,
+    genre: (parent, args, context) => {
+      const catId = args.catId;
+      const genre = genres.find((genre) => genre.id === catId);
+      if (!genre) return null;
+      return genre;
+    },
     price: () => {
       return 1465.98;
     },
     isTrainer: () => {
       return true;
+    },
+  },
+  Genre: {
+    courses: (parent, args, context) => {
+      const genreId = parent.id; // why parent?
+      return allCourses.filter((course) => course.genreId === genreId);
     },
   },
 };
